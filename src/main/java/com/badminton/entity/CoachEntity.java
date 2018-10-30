b1273d9dd9329110b3a60972c93c1e4bfd410c62
ShvedovIllia <shvedov.illia@gmail.com>
2018-10-30 14:30:56 +0200

    Init commit

--- src/main/java/com/badminton/entity/CoachEntity.java
+++ src/main/java/com/badminton/entity/CoachEntity.java
@@ -1,15 +1,22 @@
 package com.badminton.entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 
 @Entity
 @Table(name = "coach")
-@Getter
+
 public class CoachEntity {
 
     @Column(name = "first_name", nullable = false)
