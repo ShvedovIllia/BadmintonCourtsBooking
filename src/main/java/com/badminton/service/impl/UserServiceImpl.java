468ed66a2ccb11a1c4ae347ea58323f6b22aeca5
ShvedovIllia <shvedov.illia@gmail.com>
2018-11-20 17:01:28 +0200

    Init commit

--- src/main/java/com/badminton/service/impl/UserServiceImpl.java
+++ src/main/java/com/badminton/service/impl/UserServiceImpl.java
@@ -4,6 +4,7 @@ import com.badminton.domain.UserDTO;
 import com.badminton.entity.UserEntity;
 import com.badminton.repository.UserRepository;
 import com.badminton.service.UserService;
+import com.badminton.service.utils.ObjectMapperUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -16,7 +17,11 @@ public class UserServiceImpl implements UserService {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private ObjectMapperUtils modelMapper;
+
     public void addUser(UserDTO userDTO) {
+        userRepository.save(modelMapper.map(userDTO, UserEntity.class));
     }
 
     public List<UserDTO> getAllUsers() {
