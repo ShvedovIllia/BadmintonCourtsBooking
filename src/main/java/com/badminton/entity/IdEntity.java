a81f1097f08f1c94fd9dca4211b5316a62092740
ShvedovIllia <shvedov.illia@gmail.com>
2018-11-23 13:37:45 +0200

    Init commit

--- src/main/java/com/badminton/entity/IdEntity.java
+++ src/main/java/com/badminton/entity/IdEntity.java
@@ -3,10 +3,10 @@ package com.badminton.entity;
 import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.annotations.GeneratorType;
-import org.springframework.data.annotation.Id;
 
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
 
 @Getter
